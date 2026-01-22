723d23ff5a14bfc13b369dbfbd3e4ea16c069e58
Kk <iankk0806@gmail.com>
2026-01-22 23:51:01 +0800

    feat: create ec2 & vpc setting

---
+++ terraform_cloudwatch/ec2.tf
@@ -0,0 +1,53 @@
+resource "aws_instance" "cloudwatch" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  subnet_id     = module.vpc.public_subnets[0]
+
+  vpc_security_group_ids = [aws_security_group.cloudwatch-sg.id]
+  key_name               = aws_key_pair.ec2.key_name
+
+  tags = {
+    Name = "cloudwatch"
+  }
+}
+
+
+resource "aws_security_group" "cloudwatch-sg" {
+  name        = "cloudwatch-sg"
+  description = "Allow SSH, HTTP, HTTPS"
+  vpc_id = module.vpc.vpc_id
+
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform_cloudwatch/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "AWS 區域"
+  type        = string
+  default     = "ap-northeast-1"
+}
